42aa960c935c737e62009c1f7001b731590c9f88
Nagendra Kumar <nagendrakumar127@gmail.com>
2013-06-28 20:13:08 +0530

    Update fileinput.c

--- fileinput.c
+++ fileinput.c
@@ -1,39 +1,32 @@
 #include<stdio.h>
 
-void fileread()
+int main()
 {
-
-int i,k,n;
-printf("First Input Text File => No. of Rows : ");
-scanf("%d",&n);
-
-    double input[n][3];
-    char filename[256];
-    printf("First Input Text File Name : ");
-    scanf("%s",filename);
-    printf("\n");
-
-FILE *file;
-file = fopen(filename,"r");
-
-if(file==NULL)
-    printf("file not found!");
-
-for(i=0; i<n; i++)
-{
-    for(k=0; k<3; k++)
+    int i,k,n;
+    printf("First Input Text File => No. of Rows : ");
+    scanf("%d",&n);
+    
+        float input[n][12];
+        char filename[256];
+        printf("First Input Text File Name : ");
+        scanf("%s",filename);
+        printf("\n");
+    
+    FILE *file;
+    file = fopen(filename,"r");
+    
+    if(file==NULL)
+        printf("file not found!");
+    
+    for(i=0; i<n; i++)
     {
-        fscanf(file,"%lf",&input[i][k]);
-        printf("%11.10f ",input[i][k]);
-    }printf("\n");
-}
-fclose(file);
+        for(k=0; k<12; k++)
+        {
+            fscanf(file,"%f",&input[i][k]);
+            printf("%f ",input[i][k]);
+        }printf("\n");
+    }
+    fclose(file);
 
-}
-
-void main()
-{
-
- fileread();
-
+return 0;
 }
